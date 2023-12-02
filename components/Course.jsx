c29a8e797a554d3444565dc448d5ffd6db1ce0bd
Abhishekk24 <123966857+Abhishekk24@users.noreply.github.com>
2023-12-03 01:46:12 +0530

    New updates on Main page

---
+++ components/Course.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Course = ({ imageSrc, courseTitle }) => {
+  return (
+    <div className="flex-basis-[22%] md:flex-basis-[48%] lg:flex-basis-[22%] rounded-md mb-6 md:mb-0 lg:mb-8 relative overflow-hidden">
+      <img src={imageSrc} alt={courseTitle} className="w-40 h-auto mx-auto block" />
+      <h3 className="w-full text-xl block mt-5 font-bold">{courseTitle}</h3>
+    </div>
+  );
+}
+
+export default Course;
---
+++ components/Testimonial.jsx
@@ -0,0 +1,27 @@
+
+import React from 'react';
+
+const Testimonial = ({ imageSrc, content, userName, stars }) => {
+  return (
+    <div className="flex-basis-44 bg-rose-100 border rounded-md mb-5 text-left p-6 cursor-pointer">
+      <div className="flex items-center">
+        <img src={imageSrc} alt={userName} className="h-10 w-10 rounded-full mr-3" />
+        <div>
+          <p className="p-0">{content}</p>
+          <h3 className="mt-4">{userName}</h3>
+          <div className="flex">
+            {[...Array(Math.floor(stars))].map((_, index) => (
+              <i key={index} className="fa fa-star"></i>
+            ))}
+            {stars % 1 !== 0 && <i className="fa fa-star-half-o"></i>}
+            {[...Array(5 - Math.ceil(stars))].map((_, index) => (
+              <i key={index + Math.ceil(stars)} className="fa fa-star-o"></i>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonial;
