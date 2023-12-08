e4309c91cd69368fd1d3e405b36e029f107a5d91
Abhishekk24 <123966857+Abhishekk24@users.noreply.github.com>
2023-12-09 01:14:47 +0530

    UI updates

--- app/aboutus/page.jsx
+++ app/aboutus/page.jsx
@@ -9,52 +9,7 @@ const AboutUs = () => {
     <div className="bg-gray-100 min-h-screen">
       <div>
         <nav className="flex  items-center justify-between px-6 pt-4 bg-transparent w-full z-50">
-          <div className="nav-links ml-auto">
-            {/* <i className="fa fa-times" onClick={() => hideMenu()}></i> */}
-            <ul className="flex space-x-4">
-              <li className="relative">
-                <Link href="/" className="text-white text-sm px-3 py-2 ">
-                  Home
-                </Link>
-                <span className="absolute bottom-0 left-0 w-full h-0 bg-red-500 transition duration-500 ease-in-out"></span>
-              </li>
-              <li className="relative">
-                <Link href="/aboutus" className="text-white text-sm px-3 py-2">
-                  About
-                </Link>
-                <span className="absolute bottom-0 left-0 w-full h-0 bg-red-500 transition duration-500 ease-in-out"></span>
-              </li>
-              <li className="relative">
-                <Link
-                  href="#courses"
-                  data-scroll
-                  className="text-white text-sm px-3 py-2"
-                >
-                  Courses
-                </Link>
-                <span className="absolute bottom-0 left-0 w-full h-0 bg-red-500 transition duration-500 ease-in-out"></span>
-              </li>
-              <li className="relative">
-                <Link
-                  href="/contactus"
-                  className="text-white text-sm px-3 py-2"
-                >
-                  Contact Us
-                </Link>
-                <span className="absolute bottom-0 left-0 w-full h-0 bg-red-500 transition duration-500 ease-in-out"></span>
-              </li>
-            </ul>
-            {/* <div className="auth">
-          {isUserLoggedIn?(
-            <div className="button">
-              <button type='button' onClick={signOut}>Sign out</button>
-            </div>
-          ):(
-            <></>
-          )}
-        </div> */}
-          </div>
-          {/* <i className="fa fa-bars" onClick={() => showMenu()}></i> */}
+
         </nav>
       </div>
 
@@ -87,22 +42,6 @@ const AboutUs = () => {
           </button>
         </div>
       </div>
-
-      <section className="footer text-center py-8">
-        <h4 className="font-semibold mb-4">About Us</h4>
-        <p className="text-gray-700">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
-          quisquam cum. Quia dolore maxime error, eum eaque ex tenetur
-          architecto optio repellendus culpa, aliquid dignissimos possimus alias
-          porro iure hic!
-        </p>
-        <div className="icons text-red-500">
-          <i className="fab fa-facebook mx-2"></i>
-          <i className="fab fa-twitter mx-2"></i>
-          <i className="fab fa-instagram mx-2"></i>
-          <i className="fab fa-linkedin mx-2"></i>
-        </div>
-      </section>
     </div>
   );
 };
