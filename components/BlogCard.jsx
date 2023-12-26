03d78321ea8b702b0d9fc4960298e02711972b91
Abhishekk24 <123966857+Abhishekk24@users.noreply.github.com>
2023-12-26 22:16:25 +0530

    cd

--- components/BlogCard.jsx
+++ components/BlogCard.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 
 const BlogCard = ({ post }) => {
   return (
+    
     <div className='flex flex-col p-4 bg-slate-100 w-1/5 min-w-[300px] rounded-lg hover:scale-105 duration-200 ease-in-out'>
       <Link href={`/blog/${post._id}`}>
         <img src={post.coverImg} alt="not found" className='w-72 rounded-t-lg hover:brightness-75 duration-200'/>
